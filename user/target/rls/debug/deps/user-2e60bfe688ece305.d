a51d3ea694cdbdec7cc4433e2a7250aca7590ce7
Ted1166 <ogolated00@gmail.com>
2022-06-05 15:26:50 +0300

    new cli

--- user/target/rls/debug/deps/user-2e60bfe688ece305.d
+++ user/target/rls/debug/deps/user-2e60bfe688ece305.d
@@ -1,5 +1,5 @@
-c:\Users\User\Desktop\NEAR-MESSAGING_APP\user\target\rls\debug\deps\user-2e60bfe688ece305.rmeta: src\main.rs
+c:\Users\User\Desktop\NEAR-CLI_MESSAGING_APP\user\target\rls\debug\deps\user-2e60bfe688ece305.rmeta: src\main.rs
 
-c:\Users\User\Desktop\NEAR-MESSAGING_APP\user\target\rls\debug\deps\user-2e60bfe688ece305.d: src\main.rs
+c:\Users\User\Desktop\NEAR-CLI_MESSAGING_APP\user\target\rls\debug\deps\user-2e60bfe688ece305.d: src\main.rs
 
 src\main.rs:
